fc00ea278a9814d85b6e092c1a47627cb0419348
Ravishankar Rajendran <rrajendran2@yahoo.com>
2019-08-06 05:21:43 +0100

    Add files via upload
    Initial Version

---
+++ src/main/java/com/ravishankar/LearnRestTemplate/LearnRestTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.ravishankar.LearnRestTemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnRestTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnRestTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ravishankar/LearnRestTemplate/LearnRestTemplateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ravishankar.LearnRestTemplate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LearnRestTemplateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
